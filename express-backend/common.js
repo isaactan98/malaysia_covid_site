f951a53c13465775cc995dc47c0156b3483695f4
Isaac Tan <iztan98@gmail.com>
2023-12-05 00:57:05 +0800

    update: new express backend for glitch hosting

---
+++ express-backend/common.js
@@ -0,0 +1,30 @@
+parseCSV = (csvText) => {
+    // Split the CSV text into an array of lines
+    const lines = csvText.split(/\r?\n/);
+
+    // Extract headers (first row)
+    const headers = lines[0].split(',');
+
+    // Initialize an array to store the parsed CSV data
+    const parsedData = [];
+
+    // Iterate through each line starting from the second row
+    for (let i = 1; i < lines.length; i++) {
+        const fields = lines[i].split(','); // Split the line by commas
+        const rowData = {};
+
+        // Create an object with keys from the headers and values from the current row
+        for (let j = 0; j < headers.length; j++) {
+            rowData[headers[j]] = fields[j];
+        }
+
+        // Add the row object to the parsed data array
+        parsedData.push(rowData);
+    }
+
+    return parsedData;
+}
+
+module.exports = {
+    parseCSV
+}
